2e18f3480c5db13c981b3365d361e04fa256e1a2
James Eagle <jimeagle@gmail.com>
2021-05-25 10:37:37 +0100

    Option to build from local

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,16 @@
 
 echo -e "\033[4mUpdating PHP Webhooks Library...\033[0m"
 
-rm -rf ./tmp_webhooks
-(git clone https://github.com/chargehive/webhooks.git tmp_webhooks && cd ./tmp_webhooks)
+TMP_PROTO_PATH='./tmp_webhooks'
+
+if [[ $1 == "local" ]]; then
+  echo "Copying local proto repo..."
+  cp -Rf ../../webhooks/ $TMP_PROTO_PATH
+  rm -Rf $TMP_PROTO_PATH/.idea
+else
+  rm -rf ./tmp_webhooks
+  (git clone https://github.com/chargehive/webhooks.git tmp_webhooks && cd $TMP_PROTO_PATH)
+fi
 
 php ./v1.php
 rm -rf ./tmp_webhooks

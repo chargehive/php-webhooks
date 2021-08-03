c1aa6c56fe3080a674330ed46da7475afc94fba4
Jim Eagle <jimeagle@gmail.com>
2021-08-03 10:25:23 +0100

    Fix method hooks (#9)
    * Fix log
    
    * Update suspend/archive

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ echo -e "\033[4mUpdating PHP Webhooks Library...\033[0m"
 TMP_PROTO_PATH='./tmp_webhooks'
 
 if [[ $1 == "local" ]]; then
-  echo "Copying local proto repo..."
+  echo "Copying local webhooks repo..."
   cp -Rf ../../webhooks/ $TMP_PROTO_PATH
   rm -Rf $TMP_PROTO_PATH/.idea
 else

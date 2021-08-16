33f194289ab7a6989e64a5e8688a1bdd80fe4509
Jleagle <jimeagle@gmail.com>
2021-08-16 16:50:07 +0100

    Update ChargeExpired

--- src/Generated/V1/ChargeExpired.php
+++ src/Generated/V1/ChargeExpired.php
@@ -10,6 +10,41 @@ class ChargeExpired extends WebhookFoundation
    *
    * @var string
    */
-  public $chargeUuid;
+  public $chargeID;
+
+  /**
+   * The amount of the charge
+   *
+   * @var number
+   */
+  public $amount;
+
+  /**
+   * The currency of the amount
+   *
+   * @var string
+   */
+  public $currency;
+
+  /**
+   * The unix timetsamp of when the charge was attempted
+   *
+   * @var number
+   */
+  public $chargeTimestamp;
+
+  /**
+   * The charge renewal number
+   *
+   * @var number
+   */
+  public $renewalNumber;
+
+  /**
+   * The charge attempt number
+   *
+   * @var number
+   */
+  public $attemptNumber;
 
 }
